6f9a35580996a4bd8e400fa8a58b8d1c4918e9fe
HuaHua-Technophile <3118590779@qq.com>
2023-10-24 17:31:10 +0800

    修复重复两次请求用户菜单

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,4 @@
   <router-link to="/main/test">点我</router-link>
   <router-view></router-view>
 </template>
-<script setup lang="ts">
-  /* import { addMenuRouter } from "./utils/addMenuRouter/addMenuRouter";
-  addMenuRouter(); */
-</script>
+<script setup lang="ts"></script>
