43f0ea4e128406c32db2e9555b5cb388e3869bbc
HuaHua-Technophile <3118590779@qq.com>
2023-11-28 09:55:50 +0800

    升级 依赖

--- src/api/UserManagementAPI.ts
+++ src/api/UserManagementAPI.ts
@@ -5,3 +5,26 @@ export const getUserList = () => {
     method: "GET",
   });
 };
+
+interface User {
+  // businessId: Number;
+  // companyId: Number; 查询和添加用户用不到
+  companyName: String; //商户名称
+  email: String; //绑定邮箱
+  nickName: String; //部门主体名称(账号名称)
+  password: String; //密码。注册时默认填入123456
+  phoneNumber: String; //绑定手机号
+  // remark: String;
+  // sex: String;
+  status: Number; //该后台管理账号的状态
+  // userId: Number; 查询和添加用户用不到
+  userName: String; //账号
+}
+
+export const addUser = (data: User) => {
+  return instance<{ authentication: string }>({
+    url: "/system/user",
+    method: "POST",
+    data: data,
+  });
+};
--- src/views/UserManagement/index.vue
+++ src/views/UserManagement/index.vue
@@ -1,18 +1,97 @@
 <template>
-  <div
-    class="w-100 h-100"
-    style="
-      background: linear-gradient(
-        0deg,
-        rgba(0, 0, 255, 0.233),
-        rgba(255, 0, 0, 0.322)
-      );
-    ">
-    <h1>UserManagement</h1>
+  <div class="w-100 h-100">
+    <!-- 搜索用户/新建用户 -->
+    <el-form
+      :model="form"
+      ref="ruleFormRef"
+      class="d-flex flex-wrap align-items-center justify-content-around">
+      <el-form-item label="商户名称">
+        <el-input
+          clearable
+          v-model="form.companyName"
+          placeholder="每商户唯一名称"
+          :prefix-icon="renderFontIcon('bi bi-shop-window')"
+          style="min-width: 100px; max-width: 175px">
+        </el-input>
+      </el-form-item>
+      <el-form-item label="用户账号">
+        <el-input
+          clearable
+          v-model="form.email"
+          placeholder="每个后台主体唯一ID"
+          :prefix-icon="renderFontIcon('bi bi-123')"
+          style="min-width: 100px; max-width: 175px">
+        </el-input>
+      </el-form-item>
+      <el-form-item label="绑定邮箱">
+        <el-input
+          clearable
+          v-model="form.email"
+          placeholder="每个后台主体唯一邮箱"
+          :prefix-icon="renderFontIcon('bi bi-envelope')"
+          style="min-width: 100px; max-width: 175px">
+        </el-input>
+      </el-form-item>
+      <el-form-item label="绑定电话">
+        <el-input
+          clearable
+          v-model="form.email"
+          placeholder="每个后台主体唯一手机号"
+          :prefix-icon="renderFontIcon('bi bi-telephone')"
+          style="min-width: 100px; max-width: 175px">
+        </el-input>
+      </el-form-item>
+      <el-form-item label="部门主体">
+        <el-input
+          clearable
+          v-model="form.email"
+          placeholder="运营部/物流部/..."
+          :prefix-icon="renderFontIcon('bi bi-people')"
+          style="min-width: 100px; max-width: 175px">
+        </el-input>
+      </el-form-item>
+      <el-form-item><el-button>查询</el-button></el-form-item>
+      <el-form-item>
+        <el-button @click="addUserFun">添加账号</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 用户列表 -->
+    <div>
+      <div v-for="i in allUserList">{{ i }}</div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-  import { getUserList } from "@/api/UserManagementAPI.ts";
-  let res = await getUserList();
-  console.log(res);
+  import { getUserList, addUser } from "@/api/UserManagementAPI.ts";
+  import { renderFontIcon } from "@/utils/fontIcon/renderFontIcon";
+  import { FormInstance } from "element-plus";
+  import { reactive, ref } from "vue";
+
+  // 不传参数的情况下，就是获取所有用户。传参数的情况下可用作搜索
+  let allUserList = ref();
+  const getUserListFun = async () => {
+    allUserList.value = await getUserList();
+    console.log(allUserList.value);
+  };
+  getUserListFun();
+
+  // 表单部分，用于查询/添加后台管理系统的用户-------------
+  const ruleFormRef = ref<FormInstance>(); //表单实例,在验证表单规则时,需调用实例内的validate方法
+  const form = reactive({
+    companyName: "", //商户名称
+    email: "", //绑定邮箱
+    nickName: "", //部门主体名称(账号名称)
+    password: 123456, //密码。注册时默认填入123456
+    phoneNumber: "", //绑定手机号
+    userName: "", //账号
+  });
+  const addUserFun = async (formEl: FormInstance | undefined) => {
+    // 先进行表单验证
+    if (!formEl) return;
+    await formEl.validate((valid, fields) => {
+      if (valid) {
+        // getLoginInfo();
+      } else console.log("error submit!", fields);
+    });
+  };
 </script>
